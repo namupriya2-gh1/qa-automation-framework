91f80f1eed7e71ace571b373d590bd6e4036eb22
Namu Suresh <Namusuresh@Namus-MacBook-Air.local>
2026-04-06 12:31:15 -0700

    Added login simulation logic

--- first_script.py
+++ first_script.py
@@ -2,7 +2,18 @@
 
 name = "Namu"
 role = "Automation QA Engineer"
+experience = 9
 
 print(f"Hello, my name is, {name}")
 print(f"I am becoming an {role}")
- 
+print(f"I have {experience}+ years of experience in the field of software testing")
+
+
+#Login Simulation
+username = "Namu"
+password = "1234"
+
+if username == "Namu" and password == "1234":
+    print("Login successful!")
+else:
+    print("Login failed. Please check your username and password.") 
